c6673dae8e8844f204441251f0a920c426123b5e
Gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2021-06-19 08:39:05 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # installer_scripts
 Some easily auditable bash scripts for installing software using app specific apt repos.
+
+**Use at your own risk.  These are simple and well commented to encourage you to review them yourself.**
---
+++ jenkins_install.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Install Jenkins CI LTS
+#
+# Taken from:
+#   https://www.digitalocean.com/community/tutorials/how-to-install-jenkins-on-ubuntu-20-04
+# Confirmed against:
+#   https://www.jenkins.io/doc/book/installing/linux/
+
+# Get the apt key for jenkins.io
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+# Add the repo to sources.list
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+# Update the package index
+sudo apt update
+
+# Install openjdk 11, may not be installed by the Jenkins .deb file but is a required dependency
+sudo apt install -y openjdk-11-jdk
+
+# Apt install jenkins
+sudo apt install -y jenkins
